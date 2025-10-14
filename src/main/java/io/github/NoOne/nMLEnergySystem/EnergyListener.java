25b6a713063c1ef1415fb5c2f6a5ee76a9b2c770
NoOne_NoBody <hypixelnerd347@gmail.com>
2025-10-14 18:19:02 -0400

    fixed some syntax errors

--- src/main/java/io/github/NoOne/nMLEnergySystem/EnergyListener.java
+++ src/main/java/io/github/NoOne/nMLEnergySystem/EnergyListener.java
@@ -1,9 +1,10 @@
 package io.github.NoOne.nMLEnergySystem;
 
-import io.github.NoOne.nMLPlayerStats.NMLPlayerStats;
+import io.github.NoOne.nMLPlayerStats.profileSystem.ProfileManager;
 import io.github.NoOne.nMLPlayerStats.statSystem.ResetStatsEvent;
 import io.github.NoOne.nMLPlayerStats.statSystem.StatChangeEvent;
 import io.github.NoOne.nMLPlayerStats.statSystem.Stats;
+import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -12,36 +13,34 @@ import org.bukkit.event.entity.FoodLevelChangeEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 
 public class EnergyListener implements Listener {
-    private NMLPlayerStats nmlPlayerStats;
+    private ProfileManager profileManager;
 
-    public EnergyListener(NMLPlayerStats nmlPlayerStats) {
-        this.nmlPlayerStats = nmlPlayerStats;
+    public EnergyListener(NMLEnergySystem nmlEnergySystem) {
+        this.profileManager = nmlEnergySystem.getProfileManager();
     }
 
     @EventHandler
     public void onFoodLevelChange(FoodLevelChangeEvent event) {
         if (!(event.getEntity() instanceof Player player)) return;
-
-        var stats = nmlPlayerStats.getProfileManager().getPlayerProfile(player.getUniqueId()).getStats();
-
         if (player.isSprinting()) {
             event.setCancelled(true);
             return;
         }
 
+        Stats stats = profileManager.getPlayerProfile(player.getUniqueId()).getStats();
         int foodLevelChange = event.getFoodLevel() - player.getFoodLevel();
         double energyChange = foodLevelChange * (stats.getMaxEnergy() / 20);
 
-        stats.setCurrentEnergy(stats.getCurrentEnergy() + energyChange);
+        Bukkit.getPluginManager().callEvent(new StatChangeEvent(player, "currentenergy", energyChange));
     }
 
 
     @EventHandler
     public void onPlayerMove(PlayerMoveEvent event) {
         Player player = event.getPlayer();
-        Stats stats = nmlPlayerStats.getProfileManager().getPlayerProfile(player.getUniqueId()).getStats();
+        Stats stats = profileManager.getPlayerProfile(player.getUniqueId()).getStats();
 
-        if (player.isSprinting() && player.getGameMode() != GameMode.CREATIVE && player.getGameMode() != GameMode.SPECTATOR) {
+        if (player.isSprinting() && !player.isFlying()) {
             EnergyManager.useEnergy(player, stats.getMaxEnergy() / 600);
         }
     }
--- src/main/java/io/github/NoOne/nMLEnergySystem/NMLEnergySystem.java
+++ src/main/java/io/github/NoOne/nMLEnergySystem/NMLEnergySystem.java
@@ -2,50 +2,24 @@ package io.github.NoOne.nMLEnergySystem;
 
 import io.github.NoOne.nMLPlayerStats.NMLPlayerStats;
 import io.github.NoOne.nMLPlayerStats.profileSystem.ProfileManager;
-import org.bukkit.Bukkit;
-import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class NMLEnergySystem extends JavaPlugin {
-    private NMLEnergySystem instance;
-    private NMLPlayerStats nmlPlayerStats;
     private ProfileManager profileManager;
     private EnergyManager energyManager;
 
     @Override
     public void onEnable() {
-        instance = this;
-
-        Plugin plugin = Bukkit.getPluginManager().getPlugin("NMLPlayerStats");
-        if (plugin instanceof NMLPlayerStats statsPlugin) {
-            nmlPlayerStats = statsPlugin;
-            profileManager = nmlPlayerStats.getProfileManager();
-        } else {
-            getLogger().severe("Failed to find NMLPlayerStats! Disabling...");
-            getServer().getPluginManager().disablePlugin(this);
-            return;
-        }
+        profileManager = JavaPlugin.getPlugin(NMLPlayerStats.class).getProfileManager();
 
         energyManager = new EnergyManager(this);
         energyManager.energyRegenServerTask();
         energyManager.pauseEnergyRegenServerTask();
 
-        getServer().getPluginManager().registerEvents(new EnergyListener(nmlPlayerStats), this);
-    }
-
-    public NMLEnergySystem getInstance() {
-        return instance;
-    }
-
-    public NMLPlayerStats getNmlPlayerStats() {
-        return nmlPlayerStats;
+        getServer().getPluginManager().registerEvents(new EnergyListener(this), this);
     }
 
     public ProfileManager getProfileManager() {
         return profileManager;
-    }
-
-    public EnergyManager getEnergyManager() {
-        return energyManager;
     }
 }
